972d8b685be84eeaa203918e4bc069fb6736676b
rawal01singhbhati <157700273+rawal01singhbhati@users.noreply.github.com>
2024-06-16 12:40:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", default=16494736, cast=int)
+API_HASH = config("API_HASH", default=2cb7fa5859e2de684e3e10d9c049621a)
+BOT_TOKEN = config("BOT_TOKEN", default=7394117438:AAH9ZtfA1ufzgOFELtpF81xRJGOFJOZtNEo)
+SESSION = config("SESSION", default=BQD7sJAACl1e1g8UbOfrBvsx4T2rZETGwb8MKNibESFDWlKnv-4BGhXOthjIozwiSxrBy1RyWm4hfEEiHMjraX-r8gMtV_0hWmxqlcJx6AQlF5Odis4ZFVD_8bEohJZGk9XJAD64Pchz9zCY5ebWqV0yod6eDpAyRJsdCCmaeSrctK9rnK1YkYHb_VXznVo11I8BB4-Dopg1C2vYvsUJYUwXiVpYGtQhetKxaB2R_J21sRx45N-owEairZPPsPcWPYoRCXIeQPYQrrJpPZmnFbIteRVxJaZVRHJchs2IMUo_2ii_1CKNYIMYFuL2bScxho7een2HREWRHY4ZmcD5sMZB88mTtQAAAAFyZkhvAA)
+FORCESUB = config("FORCESUB", default=trail01trailsave)
+AUTH = config("AUTH", default=6214273135, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
